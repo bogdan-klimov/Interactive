3d13b7f8c12f1042c2bfa2e14f7ed8c51939e956
bogdan-klimov <bodyaklimov333@gmail.com>
2020-10-14 10:20:18 +0300

    arrow-normal

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 const gallery = document.getElementById("gallery");
+const galleryEl = document.getElementsByClassName("gallery-el");
 const angleLeft = document.getElementById("angle-left");
 const angleRight = document.getElementById("angle-right");
 
+let elWidth = window.innerWidth / 3;
+
+gallery.style.left = 0 + 'px';
+
 let step = 0;
-const move = 1020;
+
+let elsOnSlideWidth = elWidth * 3;
+
+if (window.innerWidth < 900) {
+    elsOnSlideWidth = elWidth;
+    elWidth = window.innerWidth;
+}
+
+const move = elWidth;
+
+const rightBorder = elWidth * galleryEl.length - elsOnSlideWidth;
+
+for (let i = 0; i < galleryEl.length; i++) {
+    galleryEl[i].style.minWidth = elWidth + "px";
+}
 
 const moveLeft = () => { 
     step += move;
-    if (step > 1200) {
-        step = -2040;
+    if (step > 0) {
+        step = -rightBorder;
     };
-	gallery.style.marginLeft = step + 'px';
+	gallery.style.left = step + 'px';
 };
 
 const moveRight = () => {
     step += -move;
-    if (step < -2040) {
-        step = 1020;
+    if (step < -rightBorder) {
+        step = 0;
     }
-    gallery.style.marginLeft = step + 'px';
+    gallery.style.left = step + 'px';
 };
 
 angleLeft.onclick = moveLeft;
 angleRight.onclick = moveRight;
 
+/*
+
+1 доделать стили
+2 найти ошибку в слайдере
+3 сделать слайдер в новом лендинге
+
+*/
